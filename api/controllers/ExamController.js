8f57b63bf0bab2c7e1fe2476b7da8b38eed09e62
yomnamohamed93 <engyomnaya2012@gmail.com>
2017-06-07 22:45:14 +0200

    a milestone in exam  algorithm

--- api/controllers/ExamController.js
+++ api/controllers/ExamController.js
@@ -11,14 +11,21 @@ module.exports = {
     create: function(req, res) {
         params = req.allParams();
         ExamService.create(params)
-            .then(function(exam) { res.json(exam) })
+            .then(function(exam) {
+                // res.json(exam)
+                return res.view('exam/show', { exam: exam });
+            })
             .catch(function(err) { res.serverError(err) });
         // res.json(params);
     },
+
     selectCourse: function(req, res) {
         Course.find(function(err, courses) {
             if (err) { return res.serverError(err); }
             return res.view('exam/selectCourse', { courses: courses });
         });
     },
+    show: function(req, res) {
+
+    }
 };
