b97e1459242da3f89eaa1069003700036575583a
yomnamohamed93 <engyomnaya2012@gmail.com>
2017-06-07 14:49:47 +0200

    a milestone in exam  algorithm

--- api/services/ExamService.js
+++ api/services/ExamService.js
@@ -4,13 +4,16 @@ module.exports = {
             function(resolve, reject) {
                 let examNotFinished = true;
                 Question.find({ course_id: params.course_id }, function(err, questions) {
-                    let exam;
+                    let exam = [];
                     while (examNotFinished) {
                         index = random(questions.length - 1);
                         let question = questions[index];
+                        console.log(params.Q_chapter, questions);
                         if (chapterLimitNotReached(params.Q_chapter, question.chapter, exam)) {
                             exam.push(question);
                             questions.splice(questions.indexOf(question), 1);
+                            examNotFinished = false;
+                            resolve(exam);
                         }
                         //check if the exam is ready
                     }
